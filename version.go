ad6c692bc1e68a307b785cc5ca8745d662b25631
Simon Frank <simon01frank@gmail.com>
2025-09-21 23:22:37 +0200

    chore: change version

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version represents the current version of the application
-const Version = "1.2.1"
+const Version = "1.2.3"
 
 // GitHubOwner and GitHubRepo define the GitHub repository for updates
 const (
